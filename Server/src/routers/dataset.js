db3b4d54c3c1ba24855119164b80962b9179e03e
fahadahasmi <85676923+fahadahasmi@users.noreply.github.com>
2021-10-06 14:37:33 +0530

    Add get apis of dataset

--- Server/src/routers/dataset.js
+++ Server/src/routers/dataset.js
@@ -2,7 +2,13 @@ const express = require('express');
 const router = express.Router();
 const Dataset = require('../models/Datasets.js');
 const StudentDs = require('../models/StudentDs.js');
+const cloudinary = require('cloudinary').v2;
 
+cloudinary.config({
+    api_key:process.env.API_KEY,
+    api_secret:process.env.API_SECRET,
+    cloud_name:process.env.CLOUD_NAME
+})
 router.post('/upload',async (req,res)=>{
     const {datasetName} = req.body;
     Dataset.create({
@@ -14,16 +20,45 @@ router.post('/upload',async (req,res)=>{
         res.status(500).send("Internal server error");
     })
 });
+router.get('/upload',async (req,res)=>{
+    Dataset.find({
+    }).then((Data)=>{
+        res.json(Data)
+    }).catch((err)=>{
+        console.error(err);
+        res.status(500).send("Internal server error");
+    })
+});
 
 router.post('/uploadStudentDs/:name',async (req,res)=>{
-    const {className,studentName,RollNo,Image} = req.body;
+    const {className,studentName,RollNo,Image,public_id} = req.body;
     console.log(req.params.name)
     try{
-        StudentDs.create({
-            className:req.params.name,
-            studentName,
-            RollNo,
-            Image
+        cloudinary.uploader.rename(public_id,studentName,(err,result)=>{
+            console.log(result)
+            StudentDs.create({
+                className:req.params.name,
+                studentName,
+                RollNo,
+                Image:result.url
+            }).then((data)=>{
+                res.json(data);
+            }).catch((err)=>{
+                console.log(err);
+            })
+        })
+    }
+    catch(err){
+        console.error(err);
+        res.status(500).send("Internal server error");
+    }
+});
+
+router.get('/uploadStudentDs/:name',async (req,res)=>{
+    console.log(req.params.name)
+    try{
+        StudentDs.find({
+            className:req.params.name
         }).then((data)=>{
             res.json(data);
         }).catch((err)=>{
