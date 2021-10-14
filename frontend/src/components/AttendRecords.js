a4019aadba796dbaa6293b2a737a11bbbddfde39
fahadahasmi <85676923+fahadahasmi@users.noreply.github.com>
2021-10-14 14:20:01 +0530

    Records page changes

--- frontend/src/components/AttendRecords.js
+++ frontend/src/components/AttendRecords.js
@@ -1,24 +1,26 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState,useRef } from "react";
 import Navbar from "./Screens/Navbar.js";
 import Breadcrumbs from "./Screens/Breadcrumbs.js";
 const AttendRecords = () => {
   const [selectName, setSelectName] = useState("Class Name");
   const [data, setData] = useState("");
   const [studentData, setStudentData] = useState("");
+  const showTable = useRef('');
+   let today = new Date();
+  const [selectDate, setSelectDate] = useState(today.toLocaleDateString());
   useEffect(()=>{
       getData();
       // eslint-disable-next-line 
   },[]);
 
-  
-
   function getAttendanceData() {
+    showTable.current.style.display = 'flex';
     // eslint-disable-next-line 
     if(selectName=='Class Name'){
       console.log('Select a Class');
     }
     else{
-      fetch(`http://localhost:4000/api/attend/attend/${selectName}`)
+      fetch(`http://localhost:4000/api/attend/attend/${selectName}/${selectDate}`)
       .then((res) => res.json())
       .then((resp) => {
         console.log(resp)
@@ -30,17 +32,17 @@ const AttendRecords = () => {
     }
   }
 
+  
 
   const getData = async () => {
     let result = await fetch("http://localhost:4000/api/dataset/upload");
     result = await result.json();
     console.log(result);
     setData(result);
-    
-  getAttendanceData();
   };
   
   console.log(selectName);
+  console.log(selectDate);
   return (
     <>
       <Navbar />
@@ -50,28 +52,34 @@ const AttendRecords = () => {
         <select
           id="className"
           value={selectName}
-          onChange={(e) => setSelectName(e.target.value)}
+          onChange={(e) => {
+            setSelectName(e.target.value);
+          }}
         >
           <option value="Class Name">Class Name</option>
           {Object.keys(data).map((name, i) => (
-            <option key={i} value={data[name].datasetName}>
+            <option key={i} value={data[name].datasetName} onChange={()=>{}}>
               {data[name].datasetName}
             </option>
           ))}
         </select>
+        <label htmlFor="className">Select a Date:</label>
+        <input type="date" style={{width:'135px', height:'33px'}} value={selectDate}
+          onChange={(e) => setSelectDate(e.target.value)} />
+        <button onClick={getAttendanceData}>Show data</button>
       </div>
       <div>
-      <table>
+      <table style={{display:'none'}} ref={showTable}>
             <tbody>
               <tr>
-                <th>Sr No.</th>
+                <th>Roll No.</th>
                 <th>Student's Name</th>
                 <th>Image</th>
                 <th>Attendance</th>
               </tr>
               {Object.keys(studentData).map((name,ind)=>(
                 <tr>
-      <td key={ind}>{ind + 1}</td>
+      <td key={ind}>{studentData[name].RollNo}</td>
       <td>{studentData[name].studentName}</td>
       <td><img src={studentData[name].Image} alt="Student" style={{width:60,height:60}} /></td>
     <td>Present</td>
